619c62ca3cb68ab24dd4597f5e58ee142554fceb
AmazingChris <chrisbicreme@gmail.com>
2016-06-06 18:59:11 -0400

    Update Player.java

--- Player.java
+++ Player.java
@@ -58,7 +58,19 @@ public abstract class Player{
         int e = 1;
         return(a+b+c+d+e);
     }
-   
+    
+   public void floodFill(GamePixel[][] colors, boolean[][] mark,
+                             int row, int col, GamePixel originalPixel) {
+        for(int i = 0; i < colors.length; i++){
+         for(int j = 0; j < colors[0].length; j++){
+            boolean[][] mark3 = new boolean[colors.length][colors[0].length];
+            if(isTouchingAIBlock(colors, i, j) && colors[i][j].getName().equals(originalPixel.getName())){
+               floodFillReal(colors, mark3, row, col, originalPixel);
+            }
+         }
+        }
+    }
+    
    /***
    *This will fill the blocks that are 
    *the same color and touching the chosen block.
@@ -70,8 +82,8 @@ public abstract class Player{
    *@param row the current row of the array
    *@param col the current column of the array
    *@param originalPixel used to compare colors
-   ***/
-   public void floodFill(GamePixel[][] colors, boolean[][] mark,
+   ***/ 
+   public void floodFillReal(GamePixel[][] colors, boolean[][] mark,
                              int row, int col, GamePixel originalPixel) {
         
         // make sure in bounds
