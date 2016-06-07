1980d590498039d2e469bc39b13b2335744c3593
rishi-shyamala <Rishis007@gmail.com>
2016-06-07 00:44:53 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,20 +3,61 @@ import java.awt.*;
 import javax.swing.JFrame;
 import java.awt.*;
 import java.awt.event.*;
-
+/***
+*This Main class is for the functionality for the 
+* Grid and for the AI and Buttons.
+*
+*@author Rishi
+*@version 1.1.2
+*@since 3:29 PM 6/5/16
+***/
 
 public class Main {
+   /**
+    * This is so that the program can access other files' main method
+    * */
    private static String[] args;
+  /**
+    * This is so that the frame is accesible from all 
+    * over the program
+    * */
+
    public static JFrame frame;
+      /**
+    * This is the human score
+   */
    public static JLabel human;
+      /**
+    * This is the ai score.
+    * */
    public static JLabel ai;
+   /**
+    * This is so that the panel, the grid
+    * object, can ce accesses from all over
+    * the program.
+    * */
    public static Grid mainPanel;
+      /**
+    * This is the player class for 
+    * the artificial intelligence
+   */
    public static Player comp;
+      /**
+    * This is the class for the human class for clicking the buttons.
+    * */
    public static HumanPlayer h;
+   /** this is variable for the human score 
+    * */
    public static int humanScore = 1;
+      /** this is variable for the ai score 
+    * */
    public static int aiScore = 1;
+      /** this is variable for the human score 
+    * */
    public static JPanel button;
-   
+      /**
+    * The main method.
+    * */
    public static void main(String[] args) {
       
      mainPanel = new Grid(50, 50, 17, 0.27);
@@ -86,6 +127,10 @@ public class Main {
       frame.setVisible(true);
       
     }
+    
+    /**
+     * This method checks if the previous and current screens are the same
+     * */
    public static boolean equalsColors(GamePixel[][] a, GamePixel[][] b){
       for (int i = 0; i < a.length; i++){
          for (int j = 0; j < a.length; j++){
@@ -96,6 +141,9 @@ public class Main {
       }
    return true;
    }
+   /**
+    * This method allows to copy an array for the compare method
+    * */
    
    public static GamePixel[][] copyArray(GamePixel[][] colors){
       GamePixel[][] temp = new GamePixel[colors.length][colors[0].length];
@@ -107,7 +155,9 @@ public class Main {
       }
       return temp;
    }
-   
+   /**
+    * This method allows grid to show what pixel the location is.
+    * */
    public static GamePixel whatPixel(GamePixel c){
       if(c.getName().equals("Green")) return GamePixel.GREEN;
       if(c.getName().equals("Red")) return GamePixel.RED;
@@ -117,7 +167,9 @@ public class Main {
       if(c.getName().equals("Orange")) return GamePixel.ORANGE;
       else return GamePixel.ORANGE;
    }
-   
+   /** 
+    * This is teh method that gets the player score 
+    * */
    public static int getPScores(GamePixel[][] colors){
       int scores = 0;
       for (int i = 0; i< colors.length; i++){
@@ -129,6 +181,9 @@ public class Main {
       }
       return scores;
    }
+   /**
+    * This method checks if anyone has won
+    * */
    
    public static void checkWin(){
       if (humanScore > 1250){
@@ -140,7 +195,9 @@ public class Main {
          frame.dispose();
       }
    }
-   
+   /**
+    * This method gets the score of the ai player.
+    * */
    public static int getAiScores(GamePixel[][] colors){
       int scores = 0;
       for (int i = 0; i< colors.length; i++){
@@ -152,7 +209,9 @@ public class Main {
       }
       return scores;
    }
-   
+   /**
+    *This method is the actionlistioner for the button and does the function for a red move.
+    * */
    private static class redListener implements ActionListener
    {
       private String[] args;
@@ -173,6 +232,9 @@ public class Main {
       }
       }
    }
+      /**
+    *This method is the actionlistioner for the button and does the function for a blue move.
+    * */
    private static class blueListener implements ActionListener
    {
       private String[] args;
@@ -193,7 +255,9 @@ public class Main {
       }
       }
    }   
-   
+      /**
+    *This method is the actionlistioner for the button and does the function for a green move.
+    * */
       private static class greenListener implements ActionListener
    {
       private String[] args;
@@ -214,7 +278,9 @@ public class Main {
       }
       }
    }
-
+   /**
+    *This method is the actionlistioner for the button and does the function for a yellow move.
+    * */
    private static class yellowListener implements ActionListener
    {
       private String[] args;
@@ -235,7 +301,8 @@ public class Main {
       }
       }
    }
-   
+/** This quits the game
+ * */
    private static class quitListener implements ActionListener
    {
       private String[] args;
@@ -244,7 +311,9 @@ public class Main {
       frame.dispose();
       }
    }
-   
+   /** this resets the game
+    * 
+    * */
    private static class resetListener implements ActionListener
    {
       private String[] args;
