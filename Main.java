0251ee81ed3bc7d72ceea49ad48f1be2fceafb8a
rishi-shyamala <Rishis007@gmail.com>
2016-06-06 22:01:58 -0400

    Add files via upload

--- Main.java
+++ Main.java
@@ -9,41 +9,57 @@ public class Main {
 
    public static JFrame frame;
    public static Grid mainPanel;
-   public static Player m;
+   public static Player comp;
+   public static HumanPlayer h;
+   
    public static void main(String[] args) {
       
-     mainPanel = new Grid(30, 30, 20, 0.27);
+     mainPanel = new Grid(50, 50, 17, 0.27);
      mainPanel.display();
       
       if (Difficulty.getValue() == -1){
-         m = new EasyPlayer();
+         comp = new EasyPlayer();
       }
       else if (Difficulty.getValue() == 0){
-         m = new MediumPlayer();
+         comp = new MediumPlayer();
       }
       else if (Difficulty.getValue() == 1){
-         m = new HardPlayer();
+         comp = new HardPlayer();
       }
+      
+      h = new HumanPlayer();
       
       JPanel container = new JPanel();
       container.setLayout(new BoxLayout(container, BoxLayout.Y_AXIS));
       
+      
+      
       JPanel button = new JPanel();
+      
       JButton color1 = new JButton("Red");
-      color1.addActionListener(new testListener());
+      color1.addActionListener(new redListener());
       button.add(color1);
+      
       JButton color2 = new JButton("Blue");
+      color2.addActionListener(new blueListener());
       button.add(color2);
+      
       JButton color3 = new JButton("Green");
+      color3.addActionListener(new greenListener());
       button.add(color3);
+      
       JButton color4 = new JButton("Yellow");
+      color4.addActionListener(new yellowListener());
       button.add(color4);
+      
       JButton reset = new JButton("Reset");
       reset.addActionListener(new resetListener());
       button.add(reset);
+      
       JButton quit = new JButton("Quit");
-      quit.addActionListener(new quitListener());
+      quit.addActionListener(new quitListener());     
       button.add(quit);
+
 
       
       container.add(mainPanel);
@@ -57,13 +73,51 @@ public class Main {
       frame.setVisible(true);
       
     }
-
-   private static class testListener implements ActionListener
+    
+   private static class redListener implements ActionListener
    {
       private String[] args;
       public void actionPerformed(ActionEvent e)
       {
-      m.thinkMove(mainPanel.getColors());
+      h.thinkMove(mainPanel.getColors(), GamePixel.RED);
+      mainPanel.display();
+      comp.thinkMove(mainPanel.getColors());
+      mainPanel.display();
+      }
+   }
+   
+   private static class blueListener implements ActionListener
+   {
+      private String[] args;
+      public void actionPerformed(ActionEvent e)
+      {
+      h.thinkMove(mainPanel.getColors(), GamePixel.BLUE);
+      mainPanel.display();
+      comp.thinkMove(mainPanel.getColors());
+      mainPanel.display();
+      }
+   }   
+   
+      private static class greenListener implements ActionListener
+   {
+      private String[] args;
+      public void actionPerformed(ActionEvent e)
+      {
+      h.thinkMove(mainPanel.getColors(), GamePixel.GREEN);
+      mainPanel.display();
+      comp.thinkMove(mainPanel.getColors());
+      mainPanel.display();
+      }
+   }
+
+   private static class yellowListener implements ActionListener
+   {
+      private String[] args;
+      public void actionPerformed(ActionEvent e)
+      {
+      h.thinkMove(mainPanel.getColors(), GamePixel.YELLOW);
+      mainPanel.display();
+      comp.thinkMove(mainPanel.getColors());
       mainPanel.display();
       }
    }
@@ -86,14 +140,4 @@ public class Main {
       Startup.main(args);
       }
    }
-//    private static class testListener implements ActionListener
-//    {
-//       private String[] args;
-//       public void actionPerformed(ActionEvent e)
-//       {
-//       frame.invalidate();
-//       frame.validate();
-//       frame.repaint();
-//       }
-//    }
 }
