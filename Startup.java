b5ab9b980edfb35567468d76e4c55db4bd262aae
AmazingChris <chrisbicreme@gmail.com>
2016-06-06 23:10:01 -0400

    Update Startup.java

--- Startup.java
+++ Startup.java
@@ -4,9 +4,23 @@ import java.awt.event.*;
 import javax.swing.JFrame;
 import javax.swing.JSlider;
 
+/***
+*This is essentially the Driver of the game.
+*@author Rishi
+*@version 1.1.2
+*@since 11:07 PM 6/5/16
+***/
+
 public class Startup
 {
+   /**
+    * This is the global variable of the fram of the game.
+   **/
    public static JFrame frame;
+   
+   /**
+    * This is the main method of the driver.
+   **/
    public static void main(String[] args)
    {  
       JPanel StartScreen = new JPanel();
@@ -43,6 +57,10 @@ public class Startup
       frame.getContentPane().add(StartScreen);
       frame.setVisible(true);
    }
+   
+   /**
+    * This class will be a listener for the Difficulty.java class.
+   **/
    private static class difficultyListener implements ActionListener
    {  
       private String[] args;
@@ -53,6 +71,9 @@ public class Startup
       }
    }
     
+   /**
+    * This class will be a listener for the "quit" button.
+   **/
     private static class quitListener implements ActionListener
    {
       public void actionPerformed(ActionEvent e)
@@ -60,6 +81,10 @@ public class Startup
       frame.dispose();
       }
    }
+   
+   /**
+    * This class will be a listener for iniating the game.
+   **/
    private static class playListener implements ActionListener
    {  
       private String[] args;
